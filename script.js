bcd86438266e5dc54297fa4ae2e9f55dd0b0920a
Alessandro Governale <samuele_governale@live.it>
2024-01-03 16:14:14 +0100

    feat(esercizi)esercizio 3

---
+++ script.js
@@ -0,0 +1,13 @@
+//dichiarare le variabili
+let addp = document.createElement("p")
+let text = document.createTextNode("ciao")
+ 
+
+//dichiarare le funzioni
+
+
+
+//eseguire le funzioni
+addp.appendChild(text)
+
+document.body.appendChild(addp)
